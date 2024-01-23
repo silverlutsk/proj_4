7efd20e72db4e22023b6c7e68298ad60684e13bb
silverlutsk <silverlutsk@ukr.net>
2024-01-23 22:32:53 +0200

    new

---
+++ js/main.js
@@ -0,0 +1,42 @@
+// Homework
+
+//!==========================================
+
+const day = prompt('Hello! Whats your name');
+alert('Hello ' + day + ', welcome to my website!');
+
+//!==================================================
+
+const age = prompt('Hello! Please, enter your year of birth');
+
+alert('Your age is '+ (2024 - age)+' years');
+
+//!==================================================
+
+const square = +prompt('Please, enter the length of the side of the square');
+
+alert ('The perimeter of the square is equal '+square*square+' cm')
+
+//!===================================================
+
+const radius = +prompt('Please, enter the radius of the circle');
+
+const PI = 3.14
+
+alert ('The area of ​​the circle is equal to '+PI*radius**2+' cm')
+
+//!===================================================
+
+const km = +prompt('Please, enter the distance of your trip (km)');
+
+const hours = +prompt('Please, enter the number of hours of your trip');
+
+alert ('To complete the route, your speed must be '+km/hours+' km/h')
+
+//!===================================================
+
+const dollar = +prompt('Please, enter the dollar amount');
+
+const euro = 0.92;
+
+alert ('The amount of money is equal '+dollar*euro+' euro');
